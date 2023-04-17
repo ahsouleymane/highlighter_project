04593bb5983004312707a95f78b64818723463ef
Souleymane Abdou <souleymaneah98@gmail.com>
2023-04-17 09:42:19 +0100

    second commit

--- highlight/views.py
+++ highlight/views.py
@@ -2,15 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 import json 
-import colorama
-
 # Create your views here.
-
-class bcolors:
-    OK = '\033[92m' #GREEN
-    WARNING = '\033[93m' #YELLOW
-    FAIL = '\033[91m' #RED
-    RESET = '\033[0m' #RESET COLOR
 
 def acceuil(request):
 
@@ -36,12 +28,12 @@ def acceuil(request):
         liste_bar = highlight[1]
         liste_baz = highlight[2]
 
-        # Affichage des trois chaine sur la console
         print(liste_foo)
         print(liste_bar)
         print(liste_baz)
 
-        # Affectation des valeurs à travers leurs clés start, end, comment
+        """ Affectation des valeurs à value_start_foo, value_end_foo, value_comment_foo 
+        à travers les clés start, end, comment contenue dans la chaine """
         value_start_foo = liste_foo["start"]
         value_end_foo = liste_foo["end"]
         value_comment_foo = liste_foo["comment"]
@@ -66,6 +58,8 @@ def acceuil(request):
         print(value_end_baz)
         print(value_comment_baz)
 
+        """ Affectation d'une partie de la chaine à text_*** allant de l'index 
+        value_start_*** à value_end_*** dans la chaine content """
         text_foo = content[value_start_foo: value_end_foo]
         text_bar = content[value_start_bar: value_end_bar]
         text_baz = content[value_start_baz: value_end_baz]
@@ -74,8 +68,13 @@ def acceuil(request):
         print(text_bar) 
         print(text_baz)
 
-        #text_foo = (colorama.Fore.RED + text_foo + colorama.Fore.RESET)
-        #print(text_foo)
+        text_dict_1 = {"1": text_foo}
+        text_dict_2 = {"2": text_bar}
+        text_dict_3 = {"3": text_baz}
+
+        print(text_dict_1)
+        print(text_dict_2)
+        print(text_dict_3)
 
         text_cont_1 = content[:value_start_foo]
         text_cont_2 = content[value_end_foo: value_start_bar]
@@ -87,9 +86,12 @@ def acceuil(request):
         text_cont_3_dict = {"3": text_cont_3}
         text_cont_4_dict = {"4": text_cont_4}
 
-        text_dict_1 = {"1": text_foo}
-        text_dict_2 = {"2": text_bar}
-        text_dict_3 = {"3": text_baz}
+        print(text_cont_1_dict)
+        print(text_cont_2_dict)
+        print(text_cont_3_dict)
+        print(text_cont_4_dict)
+
+        #text_dict = {"1": text_cont_1, "1": text_foo, "2": text_cont_2, "2": text_bar, "3": text_cont_3, "3": text_baz, "4": text_cont_4}
 
         data = {
             "data_dict_1": text_dict_1,
@@ -101,17 +103,9 @@ def acceuil(request):
             "data_dict_7": text_cont_4_dict,
         }
 
-        print(text_dict_1)
-        print(text_dict_2)
-        print(text_dict_3)
-
-        #content = (str.replace(content[value_start_foo: value_end_foo], text_foo_colored))
-        
-        #context = {'content': content}
         template = loader.get_template('highlight/acceuil.html')
         response = template.render(data, request)
         return HttpResponse(response)
-        #return render(request, , {'data': sorted(text_dict.items())})
     else:
         context = {}
         return render(request, 'highlight/acceuil.html', context)
